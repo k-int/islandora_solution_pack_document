3e9eeb06fda9873c9e2f51cb132b56529eecbdce
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2013-03-14 17:00:06 +0000

    Initial commit of document solution pack.

---
+++ README.txt
@@ -0,0 +1,72 @@
+
+CONTENTS OF THIS FILE
+---------------------
+
+ * summary
+ * requirements
+ * installation
+ * configuration
+ * customization
+ * troubleshooting
+ * faq
+ * contact
+ * sponsors
+
+
+SUMMARY
+-------
+
+PDF Solution Pack for Islandora
+
+Loads all required Fedora Objects, and creates an empty collection
+object to accept ingested documents of types supported by JODCOnverter
+ (currently : OpenDocument, PDF, RTF, HTML, Word, Excel, PowerPoint, and Flash).
+ 
+A PDF version is attempted to be created and stored of the file
+  (although for spreadsheets this does look a bit odd).
+
+A thumbnail and preview is also created from the created PDF if successful.
+
+REQUIREMENTS
+------------
+
+ImageMagick: http://www.imagemagick.org/script/index.php
+
+JODconverter: http://code.google.com/p/jodconverter/
+  - JODConverter requires an instance of either 
+    openoffice (http://www.openoffice.org/),
+    or libreoffice (http://www.libreoffice.org)
+    running in headless mode.
+    
+e.g. /usr/bin/soffice --nologo --nofirststartwizard --headless --norestore --invisible "--accept=socket,host=localhost,port=2002,tcpNoDelay=1;urp;" &
+
+Apache Tika
+  - should you wish to extract the text from docs.
+   This can be useful to provide text searching within the document but it not mandatory.
+   http://tika.apache.org/
+
+INSTALLATION
+------------
+
+CONFIGURATION
+-------------
+
+
+CUSTOMIZATION
+-------------
+
+
+TROUBLESHOOTING
+---------------
+
+
+F.A.Q.
+------
+
+
+CONTACT
+-------
+
+
+SPONSORS
+--------
---
+++ theme/islandora-document.tpl.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * This is the template file for the document object
+ *
+ * @TODO: Add documentation about this file and the available variables
+ */
+?>
+
+<div class="islandora-document-object islandora">
+  <div class="islandora-document-content-wrapper clearfix">
+    <?php if (isset($islandora_content)): ?>
+      <div class="islandora-document-content">
+        <?php print $islandora_content; ?>
+      </div>
+      <?php print $islandora_download_link; ?>
+    <?php endif; ?>
+  <div class="islandora-document-sidebar">
+    <?php if (!empty($dc_array['dc:description']['value'])): ?>
+      <h2><?php print $dc_array['dc:description']['label']; ?></h2>
+      <p><?php print $dc_array['dc:description']['value']; ?></p>
+    <?php endif; ?>
+    <?php if($parent_collections): ?>
+      <div>
+        <h2><?php print t('In collections'); ?></h2>
+        <ul>
+          <?php foreach ($parent_collections as $collection): ?>
+            <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+  </div>
+  </div>
+  <fieldset class="collapsible collapsed islandora-document-metadata">
+  <legend><span class="fieldset-legend"><?php print t('Extended details'); ?></span></legend>
+    <div class="fieldset-wrapper">
+      <dl class="islandora-inline-metadata islandora-document-fields">
+        <?php $row_field = 0; ?>
+        <?php foreach ($dc_array as $key => $value): ?>
+          <dt class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['label']; ?>
+          </dt>
+          <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+            <?php print $value['value']; ?>
+          </dd>
+          <?php $row_field++; ?>
+        <?php endforeach; ?>
+      </dl>
+    </div>
+  </fieldset>
+</div>
